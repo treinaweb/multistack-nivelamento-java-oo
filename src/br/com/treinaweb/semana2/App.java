d79f5c765624e33c815726d71a36abcbfe9d4024
Cleyson Lima <cleysonph@gmail.com>
2021-08-25 20:23:05 -0300

    03:22 - contrutores

--- src/br/com/treinaweb/semana2/App.java
+++ src/br/com/treinaweb/semana2/App.java
@@ -4,24 +4,23 @@ import br.com.treinaweb.semana2.classes.Diarista;
 
 public class App {
     public static void main(String[] args) {
-        var joao = new Diarista();
-        joao.nome = "João da Silva";
-        joao.telefone = "(11) 99877-7777";
-        joao.endereco = "Av Paulista, 1220";
+        var joao = new Diarista("João da Silva", "(11) 99877-7777", "Av Paulista, 1220");
 
         System.out.println("Nome: " + joao.nome);
         System.out.println("Telefone: " + joao.telefone);
         System.out.println("Endereço: " + joao.endereco);
         joao.atender("Cleyson");
 
-        var maria = new Diarista();
-        maria.nome = "Maria da Silva";
-        maria.telefone = "(11) 9877-7777";
-        maria.endereco = "Av Paulista, 1221";
+        var maria = new Diarista("Maria da Silva", "(11) 99877-7777", "Av Paulista, 1221");
 
         System.out.println("Nome: " + maria.nome);
         System.out.println("Telefone: " + maria.telefone);
         System.out.println("Endereço: " + maria.endereco);
         maria.atender("Elton");
+
+        var luis = new Diarista("Luis da Silva");
+        System.out.println("Nome: " + luis.nome);
+        System.out.println("Telefone: " + luis.telefone);
+        System.out.println("Endereço: " + luis.endereco);
     }
 }
--- src/br/com/treinaweb/semana2/classes/Diarista.java
+++ src/br/com/treinaweb/semana2/classes/Diarista.java
@@ -8,6 +8,16 @@ public class Diarista {
 
     public String endereco;
 
+    public Diarista(String nome) {
+        this.nome = nome;
+    }
+
+    public Diarista(String nome, String telefone, String endereco) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
     public void atender(String nomeCliente) {
         System.out.println("Realizando atendimento para cliente " + nomeCliente);
     }
